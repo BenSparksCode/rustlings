325ef907f0e875496378a8fef9ba95bb223a0fc1
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2024-09-05 16:23:58 +0200

    fix: use `or_default()` to improve hashmaps3

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -33,18 +33,12 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // number of goals conceded by team 1.
 
         // Update Team 1
-        let team_scores: &mut TeamScores = scores.entry(team_1_name).or_insert(TeamScores {
-            goals_scored: 0,
-            goals_conceded: 0,
-        });
+        let team_scores: &mut TeamScores = scores.entry(team_1_name).or_default();
         team_scores.goals_scored += team_1_score;
         team_scores.goals_conceded += team_2_score;
 
         // Update Team 2
-        let team_scores: &mut TeamScores = scores.entry(team_2_name).or_insert(TeamScores {
-            goals_scored: 0,
-            goals_conceded: 0,
-        });
+        let team_scores: &mut TeamScores = scores.entry(team_2_name).or_default();
         team_scores.goals_scored += team_2_score;
         team_scores.goals_conceded += team_1_score;
     }
